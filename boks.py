936cbe380097d3b5456349bce3b32664e1fb11c0
jorgenll <jorgenll@stud.ntnu.no>
2025-06-05 14:29:35 +0200

    from norwegian to english convertion i was stupid started coding in norwegian haha

--- boks.py
+++ boks.py
@@ -1,101 +1,95 @@
-
-# src/ljts/box.py
 import random
 from Molecule import Molecule
 
-class Boks:
-    """
-    Representerer en boks med molekyler fordelt i væske- og dampfaser.
-    Inkluderer periodiske randbetingelser (implisitt).
-    """
-    def __init__(self, lengde_x, lengde_y, lengde_z, tetthet_vaeske, tetthet_damp):
-        self.lx = lengde_x
-        self.ly = lengde_y
-        self.lz = lengde_z
-        self.tetthet_vaeske = tetthet_vaeske
-        self.tetthet_damp = tetthet_damp
-        self.molekyler = []
+class Box:
 
-        self._fyll_boks()
+    def __init__(self, length_x, length_y, length_z, density_liquid, density_vapor):
+        self.lx = length_x
+        self.ly = length_y
+        self.lz = length_z
+        self.density_liquid = density_liquid
+        self.density_vapor = density_vapor
+        self.molecules = []
 
-    def _fyll_boks(self):
+        self._fill_box()
+
+    def _fill_box(self):
         """
-        Fyller boksen med molekyler basert på fasedelt y-akse:
-        40% damp (bunn), 20% væske (midten), 40% damp (topp).
+        Fills the box with molecules along the y axis:
+        40% vapor, 20% liquid, 40% vapor.
         """
 
-        # Grenser mellom fasene langs y-aksen
-        y_bunn = 0
-        y_vaeske_start = self.ly * 0.4
-        y_vaeske_slutt = self.ly * 0.6
-        y_topp = self.ly
+        # Define y-boundaries of the compartments
+        y_bottom = 0
+        y_liquid_start = self.ly * 0.4
+        y_liquid_end = self.ly * 0.6
+        y_top = self.ly
 
-        # Volum av væske- og dampfaser
-        volum_vaeske = self.lx * (y_vaeske_slutt - y_vaeske_start) * self.lz
-        volum_damp_enkel = self.lx * (y_vaeske_start - y_bunn) * self.lz  # én dampfase
+        # Calculate volumes
+        volume_liquid = self.lx * (y_liquid_end - y_liquid_start) * self.lz
+        volume_vapor_single = self.lx * (y_liquid_start - y_bottom) * self.lz  # one vapor region
 
-        # Antall molekyler basert på tetthet og volum
-        antall_vaeske = int(self.tetthet_vaeske * volum_vaeske)
-        antall_damp_total = int(self.tetthet_damp * volum_damp_enkel * 2)  # to dampfaser
+        # Compute number of molecule
+        num_liquid = int(self.density_liquid * volume_liquid)
+        num_vapor_total = int(self.density_vapor * volume_vapor_single * 2)  # two vapor regions
 
-        # Fyll molekyler i hver fase
-        self.molekyler += self._generer_molekyler(antall_damp_total // 2, y_bunn, y_vaeske_start)
-        self.molekyler += self._generer_molekyler(antall_vaeske, y_vaeske_start, y_vaeske_slutt)
-        self.molekyler += self._generer_molekyler(antall_damp_total // 2, y_vaeske_slutt, y_topp)
+        # Generate molecules for each volume
+        self.molecules += self._generate_molecules(num_vapor_total // 2, y_bottom, y_liquid_start) #// for integer divisjon
+        self.molecules += self._generate_molecules(num_liquid, y_liquid_start, y_liquid_end)
+        self.molecules += self._generate_molecules(num_vapor_total // 2, y_liquid_end, y_top)
 
-    def _generer_molekyler(self, antall, y_min, y_maks):
+    def _generate_molecules(self, count, y_min, y_max):
         """
-        Lager en liste med molekyler fordelt tilfeldig innenfor angitt y-område.
+        retrn a list of molecules randomly distributed within y axis.
         """
-        nye_molekyler = []
-        for _ in range(antall):
+        new_molecules = []
+        for _ in range(count):
             x = random.uniform(0, self.lx)
-            y = random.uniform(y_min, y_maks)
+            y = random.uniform(y_min, y_max)
             z = random.uniform(0, self.lz)
-            nye_molekyler.append(Molecule(x, y, z))
-        return nye_molekyler
+            new_molecules.append(Molecule(x, y, z))
+        return new_molecules
 
-    def antall_molekyler(self):
-        return len(self.molekyler)
-    
-    def beregn_potensiell_energi(self):
+    def get_molecule_count(self):
+        return len(self.molecules)
+
+    def compute_potential_energy(self):
         """
-        Beregner total potensiell energi i boksen basert på Lennard-Jones
-        truncated-shifted (LJTS) potensialet mellom alle molekylpar.
+        Calculates the total potential energy of the system
         """
-        total_energi = 0.0                  # Akkumulert total energi
-        cutoff_avstand = 2.5
-        cutoff_r2 = cutoff_avstand ** 2    # Bruk r^2 for å unngå unødvendige kvadratrøtter
-        potensial_shift = 0.01631689       # u_LJ(2.5) for å gjøre potensialet glatt ved cutoff
+        total_energy = 0.0
+        cutoff = 2.5
+        cutoff_squared = cutoff ** 2 
+        shift_value = 0.01631689  #LJ value r = 2.5
 
-        antall_molekyler = len(self.molekyler)
+        num_molecules = len(self.molecules)
 
-        for indeks_i in range(antall_molekyler):
-            mol_i = self.molekyler[indeks_i]
+        for i in range(num_molecules):
+            mol_i = self.molecules[i]
 
-            for indeks_j in range(indeks_i + 1, antall_molekyler):
-                mol_j = self.molekyler[indeks_j]
+            for j in range(i + 1, num_molecules):
+                mol_j = self.molecules[j]
 
-                # Beregn forskyvning mellom molekylene i hver retning, sjekker hver eneste molekyl indeks i mot hver eneste molekyl ideks j + 1
+                #difference between molecules in all directions
                 dx = mol_i.x - mol_j.x
                 dy = mol_i.y - mol_j.y
                 dz = mol_i.z - mol_j.z
 
-                # Periodiske randbetingelser (minimum image convention) finner korteste avstand selvom de er "langt unna hverandre" men nærme hver sin kant
+                # Apply minimum image convention -> not to sure if we need this #######
                 dx -= round(dx / self.lx) * self.lx
                 dy -= round(dy / self.ly) * self.ly
                 dz -= round(dz / self.lz) * self.lz
 
-                # Beregn kvadrert avstand
-                avstand_r2 = dx * dx + dy * dy + dz * dz
+                # Compute squared distance
+                r_squared = dx * dx + dy * dy + dz * dz
 
-                if avstand_r2 < cutoff_r2:
-                    # Lennard-Jones potensial: u_LJ(r) = 4 * (1/r^12 - 1/r^6)
-                    invers_r2 = 1.0 / avstand_r2
-                    invers_r6 = invers_r2 ** 3
-                    invers_r12 = invers_r6 ** 2
+                if r_squared < cutoff_squared:
+                    # LJ potential: u_LJ(r) = 4 * (1/r^12 - 1/r^6)
+                    inv_r2 = 1.0 / r_squared
+                    inv_r6 = inv_r2 ** 3
+                    inv_r12 = inv_r6 ** 2
 
-                    energi_ij = 4 * (invers_r12 - invers_r6) + potensial_shift
-                    total_energi += energi_ij
+                    energy_ij = 4 * (inv_r12 - inv_r6) + shift_value
+                    total_energy += energy_ij
 
-        return total_energi
+        return total_energy
--- main.py
+++ main.py
@@ -1,32 +1,28 @@
-
-from boks import Boks
+from boks import Box
 
 def main():
-    # Dimensjoner på boksen (Lx x Ly x Lz)
-    lengde_x = 5
-    lengde_y = 40
-    lengde_z = 5
+    length_x = 5
+    length_y = 40
+    length_z = 5
 
-    # Initielle tettheter (fra oppgaveteksten)
-    tetthet_vaeske = 0.73
-    tetthet_damp = 0.02
+    #densities
+    density_liquid = 0.73
+    density_vapor = 0.02
 
-    # Lag boks med molekyler
-    boks = Boks(lengde_x, lengde_y, lengde_z, tetthet_vaeske, tetthet_damp)
-    # skille i terminal 
-    tekst_skille = "----------------------------"
-    print("\n",tekst_skille,"\n",tekst_skille,"\n")
-    # Skriv ut hvor mange molekyler som ble laget totalt
+    #Creates a box
+    box = Box(length_x, length_y, length_z, density_liquid, density_vapor)
+
+    #for easier visibility in the terminal 
+    divider = "----------------------------"
+    print("\n", divider, "\n", divider, "\n")
+
+    print(f"Total number of molecules: {box.get_molecule_count()}")
+
+    potential_energy = box.compute_potential_energy()
     
-    
-    
-    
-    print(f"Totalt antall molekyler: {boks.antall_molekyler()}")
-    potensiell_energi = boks.beregn_potensiell_energi()
-    print("Potensiell energi: ",potensiell_energi)
-    
-    
-    
-    print("\n",tekst_skille,"\n",tekst_skille)
+    print("Potential energy: ", potential_energy)
+
+    print("\n", divider, "\n", divider)
+
 if __name__ == "__main__":
     main()
