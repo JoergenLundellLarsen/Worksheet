756ff94d284019b180ce8f376b03d76625ada727
jorgenll <jorgenll@stud.ntnu.no>
2025-06-11 17:12:30 +0200

    Added monte carlo method to calculate energy loss

--- main.py
+++ main.py
@@ -1,28 +1,40 @@
 from src.ljts.boks import Box
+import matplotlib.pyplot as plt
 
 def main():
     length_x = 5
     length_y = 40
     length_z = 5
-
-    #densities
     density_liquid = 0.73
     density_vapor = 0.02
 
-    #Creates a box
     box = Box(length_x, length_y, length_z, density_liquid, density_vapor)
 
-    #for easier visibility in the terminal 
-    divider = "----------------------------"
-    print("\n", divider, "\n", divider, "\n")
+    initial_energy = box.compute_potential_energy()
+    print(f"Potential energy FØR MC: {initial_energy:.6f}")
 
-    print(f"Total number of molecules: {box.get_molecule_count()}")
+    N = box.get_molecule_count()
+    b = 1.0 / 8
+    T = 0.8  
+    sweeps = 50
+    log_interval = 1
 
-    potential_energy = box.compute_potential_energy()
-    
-    print("Potential energy: ", potential_energy)
+    energy_history = [initial_energy]
+    accept_total = 0
 
-    print("\n", divider, "\n", divider)
+    for sweep in range(1, sweeps+1):
+        accept = box.mc_sweep(b, T)
+        accept_total += accept
+
+        if sweep % log_interval == 0:
+            curr_energy = box.compute_potential_energy()
+            energy_history.append(curr_energy) #maybe use for plotting later if i have time
+            print(f"Sweep {sweep}: Pot.energy = {curr_energy:.6f}")
+
+    accept_rate = accept_total / (sweeps * N)
+    print(f"\nAcceptance ratio: {accept_rate:.3f}")
+
 
 if __name__ == "__main__":
     main()
+
